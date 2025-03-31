3bbb1793dbd00f9ad63b67c06c243e9207162408
baboyiban <chl11wq12@gmail.com>
2025-03-31 12:38:33 +0900

    Update sketch_mar31b.ino

--- sketch_mar31b/sketch_mar31b.ino
+++ sketch_mar31b/sketch_mar31b.ino
@@ -1,37 +1,29 @@
+#define PIEZO 6
 #define MAGNETIC 7
 #define PIR 8
-#define GREEN_PIN 12
-#define RED_PIN 13
-
-bool occupied = false;
 
 void setup() {
+  pinMode(PIEZO, OUTPUT);
   pinMode(MAGNETIC, INPUT_PULLUP);
   pinMode(PIR, INPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(RED_PIN, OUTPUT);
-
   Serial.begin(9600);
 }
 
 void loop() {
   int magneticValue = digitalRead(MAGNETIC);
   int pirValue = digitalRead(PIR);
-  Serial.print("magnetic State: ");
+  Serial.print("magnetic State : ");
   Serial.println(magneticValue);
   Serial.print("pir State : ");
   Serial.println(pirValue);
 
-  if (magneticValue == 0 && pirValue == 1) {
-    occupied = true;
-  } else if (magneticValue == 1 && pirValue == 1) {
-    occupied = false;
+  if (magneticValue == 1 && pirValue == 0) {
+    for (int i = 0; i < 2; i++) {
+      tone(PIEZO, 6271, 150);
+      delay(200);
+      tone(PIEZO, 4186, 150);
+      delay(200);
+    }
   }
-  if (occupied == true) {
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(RED_PIN, HIGH);
-  } else {
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(GREEN_PIN, HIGH);
-  }
+  delay(1000);
 }

69759b20ac9b2cac6fc5578a60e53119205ba7ec
baboyiban <chl11wq12@gmail.com>
2025-03-31 14:27:03 +0900

    Update sketch_mar31b.ino

--- sketch_mar31b/sketch_mar31b.ino
+++ sketch_mar31b/sketch_mar31b.ino
@@ -1,14 +1,21 @@
-#define A_OUT A0
+#include <Servo.h>
+
+#define GAS_OUT A0
 #define PIEZO 6
+#define SERVO 5
+
+Servo myservo;
 
 void setup() {
-  pinMode(A0, INPUT);
+  pinMode(GAS_OUT, INPUT);
   pinMode(PIEZO, OUTPUT);
+  myservo.attach(SERVO);
+  myservo.write(15);
   Serial.begin(9600);
 }
 
 void loop() {
-  int value = analogRead(A0);
+  int value = analogRead(GAS_OUT);
 
   if (value >= 250) {
     for (int i = 0; i < 5; i++) {
@@ -17,6 +24,9 @@ void loop() {
       tone(PIEZO, 1567, 250);
       delay(200);
     }
+    myservo.write(90);
+    delay(200);
+    myservo.detach();
   }
   Serial.print("Gas data : ");
   Serial.println(value);

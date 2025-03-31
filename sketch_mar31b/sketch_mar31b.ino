18273905dff82f3725b1ab0bc4275c8d3a8fbb68
baboyiban <chl11wq12@gmail.com>
2025-03-31 11:15:51 +0900

    Create sketch_mar31b.ino

---
+++ sketch_mar31b/sketch_mar31b.ino
@@ -0,0 +1,21 @@
+#define PIR_PIN 7
+#define PIEZO 6
+
+void setup() {
+  pinMode(PIR_PIN, INPUT);
+  pinMode(PIEZO, OUTPUT);
+}
+
+void loop() {
+  int value = digitalRead(PIR_PIN);
+  if (value == HIGH) {
+    for (int i = 0; i < 20; i++) {
+      tone(PIEZO, 523, 250);
+      delay(200);
+      tone(PIEZO, 783, 250);
+      delay(200);
+    }
+  }
+}
+
+test

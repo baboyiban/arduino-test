45ba020ff4554a169c70a52165f9787b09bc522f
baboyiban <chl11wq12@gmail.com>
2025-04-01 10:44:42 +0900

    Update sketch_mar31b.ino

--- sketch_mar31b/sketch_mar31b.ino
+++ sketch_mar31b/sketch_mar31b.ino
@@ -1,21 +1,20 @@
 #include <SoftwareSerial.h>           // 소프트웨어 시리얼 라이브러리 불러오기
 
-SoftwareSerial BTSerial(3,4);        // 소프트웨어 시리얼 객체를 3(TX),4번(RX) 으로 생성
+#define TXD 2
+#define RXD 3
 
-void setup()
-{
-  Serial.begin(9600);                // 시리얼 통신을 개시, 속도는 9600
-  BTSerial.begin(9600);              // 소프트웨어시리얼 통신 개시, 속도는 9600
+SoftwareSerial mySerial(TXD, RXD);
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
 }
 
-void loop()
-{
-  if(Serial.available())              // 시리얼 통신으로 문자가 들어오면
-  {
-    BTSerial.write(Serial.read());      // 블루투스시리얼 통신으로 발송
+void loop() {
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
   }
-  if(BTSerial.available())               // 블루투스 시리얼 통신으로 문자가 들어오면
-  {
-    Serial.write(BTSerial.read());       // 시리얼 창에 표시(시리얼 통신으로 출력)
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
   }
 }

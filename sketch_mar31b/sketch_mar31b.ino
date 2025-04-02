4d73b22f7edfb79912b3d82a5ad94448a790adbb
baboyiban <chl11wq12@gmail.com>
2025-04-02 11:49:16 +0900

    Update sketch_mar31b.ino

--- sketch_mar31b/sketch_mar31b.ino
+++ sketch_mar31b/sketch_mar31b.ino
@@ -2,11 +2,36 @@
 
 #define TXD 2
 #define RXD 3
+#define LED 13
 
 SoftwareSerial mySerial(TXD, RXD);
 
+void ledControl(int pin, int count);
+
+int ledCount = 0;
+
 void setup() {
   mySerial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
-void loop() {}
+void loop() {
+  if (mySerial.available()) {
+    byte input = mySerial.read();
+
+    switch(input) {
+      case 1:
+      ledCount++;
+      ledControl(LED, ledCount);
+      break;
+    }
+  }
+}
+
+void ledControl(int pin, int count) {
+  if (count % 2 == 1) {
+    digitalWrite(pin, HIGH);
+  } else {
+    digitalWrite(pin, LOW);
+  }
+}

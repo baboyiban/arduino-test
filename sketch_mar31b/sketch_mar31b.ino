e2a5412e7d6b38d19f84b808c622f6a5b4220ad2
baboyiban <baboyiban@gmail.com>
2025-04-04 17:35:05 +0900

    Update sketch_mar31b.ino

--- sketch_mar31b/sketch_mar31b.ino
+++ sketch_mar31b/sketch_mar31b.ino
@@ -3,17 +3,17 @@
 #include <SoftwareSerial.h> //소프트웨어 시리얼 라이브러리 추가
 #include <U8glib.h>         //u8g 라이브러리 추가
 
-#define TXD 2      // TXD를 2번 핀으로 설정
-#define RXD 3      // RXD를 3번 핀으로 설정
-#define DHTPIN 4   // 온습도 센서 out 4번 핀으로 설정
-#define SERVO 5    // 서보 모터 각도 제어 5번 핀으로 설정
-#define DC_MOTOR 6 // DC 모터 제어 6번 핀으로 설정
-#define PULSE 7    // 미세먼지 센서 입력 펄스를 7번 핀으로 인가
-#define PIR 8      // 인체 감지 센서 out 8번 핀으로 설정
-#define PIEZO 9    // 피에조 스피커 +극 9번 핀으로 설정
-#define DOOR_LED 10 // 현관 LED 등 제어 10번 핀으로 설정
-#define MAGNETIC 11 // 마그네틱 스위치 제어 11번 핀으로 설정
-#define ROOM_LED 12 // 방 LED 제어 12번 핀으로 설정
+#define TXD 2             // TXD를 2번 핀으로 설정
+#define RXD 3             // RXD를 3번 핀으로 설정
+#define DHTPIN 4          // 온습도 센서 out 4번 핀으로 설정
+#define SERVO 5           // 서보 모터 각도 제어 5번 핀으로 설정
+#define DC_MOTOR 6        // DC 모터 제어 6번 핀으로 설정
+#define PULSE 7           // 미세먼지 센서 입력 펄스를 7번 핀으로 인가
+#define PIR 8             // 인체 감지 센서 out 8번 핀으로 설정
+#define PIEZO 9           // 피에조 스피커 +극 9번 핀으로 설정
+#define DOOR_LED 10       // 현관 LED 등 제어 10번 핀으로 설정
+#define MAGNETIC 11       // 마그네틱 스위치 제어 11번 핀으로 설정
+#define ROOM_LED 12       // 방 LED 제어 12번 핀으로 설정
 #define LIVINGROOM_LED 13 // 거실 LED 제어 13번 핀으로 설정
 #define OUTPUT_VOLTAGE A0 // 미세먼지 센서 출력 전압 A0 핀으로 설정
 #define GAS_OUT A1        // 가스 센서 아날로그 출력 A1으로 설정
@@ -26,17 +26,17 @@ Servo myservo;                     // 서보 모터 객체 선언
 
 void ledControl(int pin, int count); // 매개 변수로 제어 핀과 count 횟수를 가진
                                      // ledControl() 함수 선언
-void motor_Control(byte dcStep); // DC 모터 동작과 관련된 motor_Control 함수
-                                 // 선언
+void motor_Control(byte dcStep);     // DC 모터 동작과 관련된 motor_Control 함수
+                                     // 선언
 
 int LivingroomLedCount = 0; // 거실 LED 제어 시 발생한 스위칭 횟수를 저장
-int roomLedCount = 0; // 방 LED 제어 시 발생한 스위칭 횟수를 저장
-byte motorStep = 0;   // 모터 속도 단계를 저장
-float preVoltage = 0; // 0~1023 범위의 출력 전압
-float voltage = 0;    // 0~5 범위의 출력 전압
-float dustDensity = 0;    // 미세먼지 농도 수치
-float sumDustDensity = 0; // 미세먼지 농도 수치 합
-float avgDustDensity = 0; // 미세먼지 농도 수치 평균
+int roomLedCount = 0;       // 방 LED 제어 시 발생한 스위칭 횟수를 저장
+byte motorStep = 0;         // 모터 속도 단계를 저장
+float preVoltage = 0;       // 0~1023 범위의 출력 전압
+float voltage = 0;          // 0~5 범위의 출력 전압
+float dustDensity = 0;      // 미세먼지 농도 수치
+float sumDustDensity = 0;   // 미세먼지 농도 수치 합
+float avgDustDensity = 0;   // 미세먼지 농도 수치 평균
 
 void setup() {
   dht.begin();                     // 온습도 센서 동기화
@@ -63,7 +63,7 @@ void loop() {
       MAGNETIC); // 마그네틱 스위치의 출력 신호를 magneticValue 변수에 저장
   int gasValue = analogRead(GAS_OUT); // 아날로그 데이터를 읽어 value 변수에
                                       // 저장
-  Serial.print("Pir state :"); // 인체 감지 센서 상태 확인
+  Serial.print("Pir state :");        // 인체 감지 센서 상태 확인
   Serial.print(pirValue);
   Serial.print("\t");
   Serial.print("magnetic state :"); // 마그네틱 스위치 상태 확인
@@ -104,7 +104,7 @@ void loop() {
     myservo.detach();   // 서보 모터 출력 신호 정지
   }
 
-  float humidity = dht.readHumidity(); // 습도 데이터를 읽어와 변수에 저장
+  float humidity = dht.readHumidity();       // 습도 데이터를 읽어와 변수에 저장
   float temperature = dht.readTemperature(); // 온도 데이터를 읽어와 변수에 저장
   if (isnan(humidity) ||
       isnan(temperature)) { // 데이터를 읽어오지 못했을 경우 아래 메시지 출력
@@ -117,14 +117,14 @@ void loop() {
     digitalWrite(PULSE, LOW); // 입력 펄스 인가
     delayMicroseconds(280);   // 0.28ms 대기
     preVoltage = analogRead(
-        OUTPUT_VOLTAGE); // A0 핀으로부터 데이터를 읽어 preVoltage에 저장
+        OUTPUT_VOLTAGE);       // A0 핀으로부터 데이터를 읽어 preVoltage에 저장
     delayMicroseconds(40);     // 0.04ms 대기
     digitalWrite(PULSE, HIGH); // 입력 펄스 종료
     delayMicroseconds(9680);   // 9.68ms 대기
     voltage = preVoltage * 5.0 /
               1024.0; // 0~5 범위 전압 값으로 변환 후 voltage에 저장
     dustDensity =
-        (voltage - 0.3) / 0.005; // 미세먼지 농도 수치 dustDensity에 저장
+        (voltage - 0.3) / 0.005;   // 미세먼지 농도 수치 dustDensity에 저장
     sumDustDensity += dustDensity; // 미세먼지 농도 수치 합계
     delay(10);                     // 데이터 계산 간 10ms 대기
   }
@@ -135,21 +135,21 @@ void loop() {
   do {
     u8g.setFont(u8g_font_fub14); // 온도, 습도 폰트지정
     u8g.setPrintPos(5, 20);      // 온도 데이터 출력 커서 설정
-    u8g.print(temperature, 1); // 온도 데이터(소수점 첫째 자리) 출력
+    u8g.print(temperature, 1);   // 온도 데이터(소수점 첫째 자리) 출력
     u8g.print("\xb0"
-              "C");          // 온도 기호(°C) 출력
-    u8g.setPrintPos(70, 20); // 습도 데이터 출력 커서 설정
-    u8g.print(humidity, 1);  // 습도 데이터(소수점 첫째 자리) 출력
-    u8g.print("%");          // 습도 기호(%) 출력
-    u8g.setFont(u8g_font_fub20); // 미세먼지 농도 수치 폰트지정
-    u8g.setPrintPos(40, 55); // 미세먼지 농도 수치 출력 커서 설정
+              "C");               // 온도 기호(°C) 출력
+    u8g.setPrintPos(70, 20);      // 습도 데이터 출력 커서 설정
+    u8g.print(humidity, 1);       // 습도 데이터(소수점 첫째 자리) 출력
+    u8g.print("%");               // 습도 기호(%) 출력
+    u8g.setFont(u8g_font_fub20);  // 미세먼지 농도 수치 폰트지정
+    u8g.setPrintPos(40, 55);      // 미세먼지 농도 수치 출력 커서 설정
     u8g.print(avgDustDensity, 1); // 미세먼지 농도 수치(소수점 첫째 자리) 출력
   } while (u8g.nextPage()); // picture loop의 끝
 
   mySerial.print(temperature, 1); // 소수점 첫째 자리까지 온도 데이터 출력
   mySerial.print(" ");            // 각 데이터를 공백으로 구분
-  mySerial.print(humidity, 1); // 소수점 첫째 자리까지 습도 데이터 출력
-  mySerial.print(" ");         // 각 데이터를 공백으로 구분
+  mySerial.print(humidity, 1);    // 소수점 첫째 자리까지 습도 데이터 출력
+  mySerial.print(" ");            // 각 데이터를 공백으로 구분
   mySerial.println(avgDustDensity,
                    1); // 소수점 첫째 자리까지 미세먼지 데이터 출력
 
@@ -161,7 +161,7 @@ void loop() {
     if (input >= 30) // DC 모터 제어와 관련된 데이터 발생 시
     {
       motorStep = input % 10; // 모터 속도를 나타내는 단계 처리
-      input = input / 10; // 모터 제어와 관련된 고유 번호 3 처리
+      input = input / 10;     // 모터 제어와 관련된 고유 번호 3 처리
     }
 
     switch (input) // input 변수의 값에 맞는 case 문 실행
@@ -193,7 +193,7 @@ void ledControl(int pin, int count) // ledControl() 함수 정의
   {
     digitalWrite(pin, HIGH); // LED를 제어하는 핀에 HIGH 신호를 발생시켜 LED
                              // 점등
-  } else // count 값이 짝수일 때
+  } else                     // count 값이 짝수일 때
   {
     digitalWrite(pin, LOW); // LED를 제어하는 핀에 LOW 신호를 발생시켜 LED 소등
   }

084d5493bc27b352edf9c0278f625c5d9f533518
baboyiban <baboyiban@gmail.com>
2025-04-04 17:33:34 +0900

    Update sketch_mar31b.ino

--- sketch_mar31b/sketch_mar31b.ino
+++ sketch_mar31b/sketch_mar31b.ino
@@ -1,18 +1,22 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo myservo; // create servo object to control a servo
+// twelve servo objects can be created on most boards
 
-int pos = 0;
+int pos = 0; // variable to store the servo position
 
-void setup() { myservo.attach(6); }
+void setup() {
+  myservo.attach(6); // attaches the servo on pin 9 to the servo object
+}
 
 void loop() {
-  for (pos = 0; pos <= 120; pos += 1) {
-    myservo.write(pos);
-    delay(15);
+  for (pos = 0; pos <= 60; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos); // tell servo to go to position in variable 'pos'
+    delay(50);          // waits 15ms for the servo to reach the position
   }
-  for (pos = 120; pos >= 0; pos -= 1) {
-    myservo.write(pos);
-    delay(15);
+  for (pos = 60; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos); // tell servo to go to position in variable 'pos'
+    delay(50);          // waits 15ms for the servo to reach the position
   }
 }
